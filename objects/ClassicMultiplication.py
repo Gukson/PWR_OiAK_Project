4db4429bb3276cfbde08d2c3060bf46de8a76e0b
Jakub Walica <jakubw4444@gmail.com>
2024-04-07 18:22:10 +0200

    ClassicMultiplication multiply function

--- objects/ClassicMultiplication.py
+++ objects/ClassicMultiplication.py
@@ -1,3 +1,17 @@
 class ClassicMultiplication:
     multiplicand = []
     multiplier = []
+
+
+def multiply(multiplicand, multiplier):
+    multiplicand_digits = [int(digit) for digit in str(multiplicand)]
+
+    multiplier_digits = [int(digit) for digit in str(multiplier)]
+
+    results = [0] * (len(multiplicand_digits) + len(multiplier_digits))
+
+    for i in range(len(multiplicand_digits)-1, -1, -1):
+        for j in range(len(multiplier_digits)-1, -1, -1):
+            results[i + j + 1] += multiplicand_digits[i] * multiplier_digits[j]
+            results[i + j] += results[i + j + 1] // 10
+            results[i + j + 1] %= 10
