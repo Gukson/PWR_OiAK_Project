3f0e81b013fce01a13b9264e39bbadbc9122ac08
Jakub Walica <jakubw4444@gmail.com>
2024-05-06 15:33:07 +0200

    generator

--- main.py
+++ main.py
@@ -9,8 +9,33 @@
 
 from objects.sos_multiply import sos_multiply
 from objects.cios_multiply import cios_multiply
+from objects.generator import generate_numbers
+from objects.generator import read_file
+import time
 
 print(sos_multiply('123', '45'))
 print(cios_multiply('123', '45'))
+
+amount = 200
+generate_numbers(amount, 'random_numbers.txt')
+numbers = read_file('random_numbers.txt')
+for pair in numbers:
+    start_time = time.time()
+    sos_multiply(pair[0], pair[1])
+    end_time = time.time()
+
+    execution_time = end_time - start_time
+
+    with open('sos_multiply.txt', 'a') as file:
+        file.write(f"{execution_time}\n")
+
+    start_time = time.time()
+    cios_multiply(pair[0], pair[1])
+    end_time = time.time()
+
+    execution_time = end_time - start_time
+
+    with open('cios_multiply.txt', 'a') as file:
+        file.write(f"{execution_time}\n")
 
 
---
+++ objects/generator.py
@@ -0,0 +1,21 @@
+import random
+
+
+def generate_numbers(amount, file_path):
+    with open(file_path, 'w') as file:
+        for _ in range(amount):
+            random_number = random.randint(10**10, 10**16)
+            file.write(str(random_number) + '\n')
+
+
+def read_file(file_path):
+    pairs = []
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for i in range(0, len(lines), 2):
+            if i+1 < len(lines):
+                pairs.append((lines[i].strip(), lines[i+1].strip()))
+
+    return pairs
+
+
